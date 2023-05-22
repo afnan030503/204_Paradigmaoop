b867e2aa469bf672051379b0ef9b6e589ef20a00
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-22 20:19:04 +0700

    memanggil variabel

---
+++ mainJM/jantung.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef JANTUNG_H
+#define JANTUNG_H
+using namespace std;
+#include <iostream>
+#include "jantung.h"
+class jantung {
+public:
+	jantung() {
+		cout << "jantung dihidupkan\n";
+	}
+	~jantung() {
+		cout << "jantung dimatikan\n";
+	}
+};
+#endif 
