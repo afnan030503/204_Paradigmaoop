3b99cf068f20420c134d13e2a687c5acf96f682f
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-22 19:41:31 +0700

    varuabel

--- Paradigma/Paradigma.cpp
+++ Paradigma/Paradigma.cpp
@@ -12,5 +12,15 @@ public:
 	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
 
 	}
-
+	~mahasiswa() {
+		cout << "Id = " << id << endl;
+		cout << "Nama = " << nama << endl;
+		cout << "Nilai = " << nilai << endl;
+}
 };
+
+int main() {
+	mahasiswa mhs(12, "asroni", 90.5);
+
+	return 0;
+}
